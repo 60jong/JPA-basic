c605f4c44b14dc1c759ca264d9014dfc015fd997
60jong <rudwhd515@gmail.com>
2023-09-10 19:23:08 +0900

    jpql select by dto

--- src/main/java/org/example/jpql/JpqlMain.java
+++ src/main/java/org/example/jpql/JpqlMain.java
@@ -1,9 +1,12 @@
 package org.example.jpql;
 
+import org.example.jpql.domain.dto.UserDto;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpqlMain {
     public static void main(String[] args) {
@@ -12,6 +15,11 @@ public class JpqlMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
+        // code
+        List<UserDto> userDtos = em.createQuery("select new org.example.jpql.domain.dto.UserDto(u.name, u.age) from User u", UserDto.class)
+                .getResultList();
+
+        System.out.println(userDtos);
         tx.commit();
         em.close();
         emf.close();
---
+++ src/main/java/org/example/jpql/domain/dto/UserDto.java
@@ -0,0 +1,11 @@
+package org.example.jpql.domain.dto;
+
+public class UserDto {
+    private String name;
+    private int age;
+
+    public UserDto(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
